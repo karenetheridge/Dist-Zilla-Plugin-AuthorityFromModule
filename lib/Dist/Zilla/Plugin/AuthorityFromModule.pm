71a813bd361dbe49c52c1bce18d2bde8d2da61f4
Karen Etheridge <ether@cpan.org>
2015-07-26 18:36:39 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/AuthorityFromModule.pm
+++ lib/Dist/Zilla/Plugin/AuthorityFromModule.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::AuthorityFromModule;
 # ABSTRACT: Add metadata to your distribution indicating what module to copy PAUSE permissions from
 # KEYWORDS: distribution metadata authority permissions PAUSE users
-# vim: set ts=8 sts=4 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.007';
 
