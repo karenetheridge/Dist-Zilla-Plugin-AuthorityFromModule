2839a266ca6974367a9fe4593d723c99e8df70b6
Karen Etheridge <ether@cpan.org>
2015-09-03 20:36:41 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/AuthorityFromModule.pm
+++ lib/Dist/Zilla/Plugin/AuthorityFromModule.pm
@@ -148,14 +148,6 @@ This config is optional; it defaults to the L<main module|Dist::Zilla/main_modul
 
 =for Pod::Coverage metadata
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-AuthorityFromModule>
-(or L<bug-Dist-Zilla-Plugin-AuthorityFromModule@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-AuthorityFromModule@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
